2fb8e3005929a16ab8460086d47b70d8ac52be52
safagrech <grechsafa23@gmail.com>
2021-06-30 18:25:34 +0100

    week 2 ex2

---
+++ week2ex2.js
@@ -0,0 +1,129 @@
+/*
+Write a function that:
+- takes an array of strings as input
+- removes any spaces in the beginning or end of the strings
+- removes any forward slashes (/) in the strings
+- makes the string all lowercase
+*/
+function tidyUpString(strArr) {
+let tab= strArr.map(function (x) { return x.replace(/[/]/g,"").trim().toLowerCase();})
+return tab ; 
+}
+
+/*
+Complete the function to check if the variable `num` satisfies the following requirements:
+- is a number
+- is even
+- is less than or equal to 100
+Tip: use logical operators
+*/
+
+function validate(num) {
+    if ( (typeof num === "number") && (num <= 100) && (num% 2 == 0)) { return true}
+    else return false ; 
+}
+
+/* 
+Write a function that removes an element from an array
+The function must:
+- NOT change the original array
+- return a new array with the item removed
+- remove the item at the specified index
+*/
+
+function remove(arr, index) {
+   
+   let tab = arr.slice(0,index).concat(arr.slice(index+1 ,arr.length))
+  return tab; // complete this statement
+}
+
+/*
+Write a function that:
+- takes an array of numbers as input
+- returns an array of strings formatted as percentages (e.g. 10 => "10%")
+- the numbers must be rounded to 2 decimal places
+- numbers greater 100 must be replaced with 100
+*/
+
+function formatPercentage(arr) {
+    let tab= arr.map(function (x) { if (x>100){ x=100}
+    x=Math.round(x *100) / 100 ;
+    x=x+"%";
+    return x ;})
+    return tab ;
+  
+}
+
+/* ======= TESTS - DO NOT MODIFY ===== */
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+
+  return true;
+}
+
+function test(test_name, expr) {
+  let status;
+  if (expr) {
+    status = "PASSED";
+  } else {
+    status = "FAILED";
+  }
+
+  console.log(`${test_name}: ${status}`);
+}
+
+test(
+  "tidyUpString function works - case 1",
+  arraysEqual(tidyUpString(["/Daniel ", "irina ", " Gordon", "ashleigh "]), [
+    "daniel",
+    "irina",
+    "gordon",
+    "ashleigh"
+  ])
+);
+test(
+  "tidyUpString function works - case 2",
+  arraysEqual(
+    tidyUpString([" /Sanyia ", " Michael ", "AnTHonY ", "   Tim   "]),
+    ["sanyia", "michael", "anthony", "tim"]
+  )
+);
+
+test("validate function works - case 1", validate(10) === true);
+test("validate function works - case 2", validate(18) === true);
+test("validate function works - case 3", validate(17) === false);
+test("validate function works - case 4", validate("Ten") === false);
+test("validate function works - case 5", validate(108) === false);
+
+test(
+  "remove function works - case 1",
+  arraysEqual(remove([10, 293, 292, 176, 29], 3), [10, 293, 292, 29])
+);
+test(
+  "remove function works - case 1",
+  arraysEqual(remove(["a", "b", "c", "d", "e", "f", "g"], 6), [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f"
+  ])
+);
+
+test(
+  "formatPercentage function works - case 1",
+  arraysEqual(formatPercentage([23, 18, 187.2, 0.372]), [
+    "23%",
+    "18%",
+    "100%",
+    "0.37%"
+  ])
+);
